c6d27e6c03e941cf801da5d789de6f2ff993cfa5
admin <stychinskiisv@gmail.com>
2025-08-07 13:00:28 +0300

    WIP: 1.0

--- src/main.ts
+++ src/main.ts
@@ -1,31 +1,10 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { GlobalModule } from './global.module';
 
 async function bootstrap() {
-    const app = await NestFactory.create(AppModule);
-
-    app.useGlobalPipes(
-        new ValidationPipe({
-            whitelist: true,
-            forbidNonWhitelisted: true,
-            transform: true,
-        }),
-    );
-
-    const config = new DocumentBuilder()
-        .setTitle('Авторизация API')
-        .setDescription('API для авторизации с JWT и ролями')
-        .setVersion('1.0')
-        .addBearerAuth()
-        .build();
-
-    const document = SwaggerModule.createDocument(app, config);
-    SwaggerModule.setup('api', app, document);
-
-    const port = 3011;
-    await app.listen(process.env.PORT ?? port);
+    const app = await NestFactory.create(GlobalModule);
+    const port = process.env.PORT ?? 3011;
+    await app.listen(port);
     console.log(`Server started on port ${port}`);
 }
 
